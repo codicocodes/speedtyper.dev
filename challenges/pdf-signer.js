a0a4690b58f7c3fcce3b5673255f4449ae67c29a
Codico <>
2021-04-10 21:42:31 +0200

    Prettier diff

--- challenges/pdf-signer.js
+++ challenges/pdf-signer.js
@@ -1,7 +1,4 @@
-const source = [
-  "file",
-  "index",
-];
+const source = ["file", "index"];
 
 module.exports = [
   ...source.map((name) => ({
@@ -9,8 +6,7 @@ module.exports = [
     project: "PDF-Signer",
     projectUrl: "https://github.com/bennodev19/pdf-signer",
     license: "MIT",
-    licenseUrl:
-      "https://github.com/bennodev19/pdf-signer/blob/master/LICENSE",
+    licenseUrl: "https://github.com/bennodev19/pdf-signer/blob/master/LICENSE",
     source: `https://raw.githubusercontent.com/bennodev19/pdf-signer/master/src/${name}.ts`,
     language: "TypeScript",
   })),
