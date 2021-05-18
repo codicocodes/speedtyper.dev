53d174cdfb0705f5007436942ccc033eea3edbe3
shitzuu <ra.chomczyk@gmail.com>
2021-05-18 13:57:36 +0200

    :coffe:: Adding more Java snippets. :pen:

---
+++ challenges/unirest-java.js
@@ -0,0 +1,177 @@
+module.exports = [
+    {
+        name: "JsonPatch.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/JsonPatch.java",
+    },
+    {
+        name: "JsonPatchItem.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/JsonPatchItem.java",
+    },
+    {
+        name: "JsonObjectMapper.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/JsonObjectMapper.java",
+    },
+    {
+        name: "JsonNode.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/JsonNode.java",
+    },
+    {
+        name: "Callback.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/Callback.java",
+    },
+    {
+        name: "CallbackFuture.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/CallbackFuture.java",
+    },
+    {
+        name: "JsonResponse.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/JsonResponse.java",
+    },
+    {
+        name: "UniByteArrayBody.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/UniByteArrayBody.java",
+    },
+    {
+        name: "Unirest.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/Unirest.java",
+    },
+    {
+        name: "UnirestInstance.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/UnirestInstance.java",
+    },
+    {
+        name: "RawResponseBase.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/RawResponseBase.java",
+    },
+    {
+        name: "RequestSummary.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/RequestSummary.java",
+    },
+    {
+        name: "ResponseSummary.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/ResponseSummary.java",
+    },
+    {
+        name: "BaseResponse.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/BaseResponse.java",
+    },
+    {
+        name: "BasicResponse.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/BasicResponse.java",
+    },
+    {
+        name: "BaseRequest.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/BaseRequest.java",
+    },
+    {
+        name: "AsyncClient.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/AsyncClient.java",
+    },
+    {
+        name: "JSONElement.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/json/JSONElement.java",
+    },
+    {
+        name: "JSONException.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/json/JSONException.java",
+    },
+    {
+        name: "JSONObject.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/json/JSONObject.java",
+    },
+    {
+        name: "JSONPointer.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/json/JSONPointer.java",
+    },
+    {
+        name: "JSONPointerException.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/json/JSONPointerException.java",
+    },
+    {
+        name: "NullObject.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/json/NullObject.java",
+    },
+    {
+        name: "ToObjectMapper.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/json/ToObjectMapper.java",
+    },
+    {
+        name: "JSONArray.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/json/JSONArray.java",
+    },
+    {
+        name: "ApacheClient.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/apache/ApacheClient.java",
+    },
+    {
+        name: "ApacheAsyncClient.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/apache/ApacheAsyncClient.java",
+    },
+    {
+        name: "ApacheAsyncConfig.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/apache/ApacheAsyncConfig.java",
+    },
+    {
+        name: "MonitoringStream.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/apache/MonitoringStream.java",
+    },
+    {
+        name: "MonitoringFileBody.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/apache/MonitoringFileBody.java",
+    },
+    {
+        name: "MonitoringStreamBody.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/apache/MonitoringStreamBody.java",
+    },
+    {
+        name: "RequestConfigFactory.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/apache/RequestConfigFactory.java",
+    },
+    {
+        name: "RequestPrep.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/apache/RequestPrep.java",
+    },
+    {
+        name: "Body.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/Body.java",
+    },
+    {
+        name: "BodyPart.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/BodyPart.java",
+    },
+    {
+        name: "ByteArrayPart.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/ByteArrayPart.java",
+    },
+    {
+        name: "FilePart.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/FilePart.java",
+    },
+    {
+        name: "FileResponse.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/FileResponse.java",
+    },
+    {
+        name: "Header.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/Header.java",
+    },
+    {
+        name: "HeaderNames.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/HeaderNames.java",
+    },
+    {
+        name: "Headers.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/Headers.java",
+    },
+    {
+        name: "HttpMethod.java",
+        source: "https://raw.githubusercontent.com/Kong/unirest-java/main/unirest/src/main/java/kong/unirest/HttpMethod.java",
+    },
+].map((file) => ({
+    ...file,
+    project: "unirest-java",
+    projectUrl: "https://github.com/Kong/unirest-java",
+    licenseUrl: "https://github.com/Kong/unirest-java/blob/main/LICENSE",
+    license: "MIT",
+    language: "Java",
+}));
