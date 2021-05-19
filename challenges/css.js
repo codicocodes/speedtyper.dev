fa3e4fec6e2d2b657422dabfdc6e05b64a4969b8
Paul C <paulcollinsaf@gmail.com>
2021-05-20 08:46:10 +1000

    remove unnecessary license from css.js

--- challenges/css.js
+++ challenges/css.js
@@ -2,9 +2,7 @@ module.exports = [
   {
     name: "cssGrid",
     project: "CSS Grid",
-    projectUrl: "https://github.com/mdn/content/blob/main/files/en-us/web/css/css_grid_layout/index.html",
-    license: "CC-BY-SA-2.5",
-    licenseUrl: "https://github.com/mdn/content/blob/main/LICENSE.md",
+    projectUrl: "https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout#css",
     source: "https://gist.githubusercontent.com/severalpens/95b8574dce0125fe397fd252e5932ff6/raw/832b8761c3d57aee6047f51fc91fe757177c1a71/cssgrid.css",
     language: "CSS",
   }
