7bd96987fd84404f5ad6cdb054f50de860ad4457
Praful Gupta <prafulgupta6@gmail.com>
2021-05-22 23:13:25 +0530

    RAWWWW

--- challenges/wordoku_csp.js
+++ challenges/wordoku_csp.js
@@ -1,11 +1,11 @@
 module.exports = [
     {
         name: "CSP.py",
-        source: "https://github.com/prafgup/Wordoku_AI_CSP_Solver/blob/master/CSP.py",
+        source: "https://raw.githubusercontent.com/prafgup/Wordoku_AI_CSP_Solver/master/CSP.py",
     },
     {
         name: "WordokuSolver_Backtracking.py",
-        source: "https://github.com/prafgup/Wordoku_AI_CSP_Solver/blob/master/WordokuSolver_Backtracking.py",
+        source: "https://raw.githubusercontent.com/prafgup/Wordoku_AI_CSP_Solver/master/WordokuSolver_Backtracking.py",
     }
   ].map((file) => ({
     ...file,
