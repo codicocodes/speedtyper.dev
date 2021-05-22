f0ba39fd84576bc787be7ae906c47392dcd3d964
Paul C <paulcollinsaf@gmail.com>
2021-05-23 08:52:05 +1000

    add css.js

---
+++ challenges/css.js
@@ -0,0 +1,11 @@
+module.exports = [
+    {
+      name: "cssGrid",
+      project: "CSS Grid",
+      projectUrl: "https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout#css",
+      source: "https://gist.githubusercontent.com/severalpens/95b8574dce0125fe397fd252e5932ff6/raw/832b8761c3d57aee6047f51fc91fe757177c1a71/cssgrid.css",
+      language: "CSS",
+    }
+  ]
+
+  //TODO: Add to index.js
