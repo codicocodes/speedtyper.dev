d3f604323c23e31b8c14a51e62ccbf479b23d5ab
Leon Tepe <27787342+leontepe@users.noreply.github.com>
2022-03-13 10:47:57 +0100

    added pathfinder

---
+++ challenges/pathfinder.js
@@ -0,0 +1,31 @@
+module.exports = [
+    {
+        name: "geometry_rect",
+        project: "Pathfinder 3",
+        projectUrl: "https://github.com/servo/pathfinder",
+        license: "MIT",
+        licenseUrl: "https://github.com/servo/pathfinder/blob/master/LICENSE-MIT",
+        source: "https://github.com/servo/pathfinder/blob/master/geometry/src/rect.rs",
+        language: "Rust",
+    },
+    {
+        name: "geometry_vector",
+        project: "Pathfinder 3",
+        projectUrl: "https://github.com/servo/pathfinder",
+        license: "MIT",
+        licenseUrl: "https://github.com/servo/pathfinder/blob/master/LICENSE-MIT",
+        source:
+            "https://github.com/servo/pathfinder/blob/master/geometry/src/vector.rs",
+        language: "Rust",
+    },
+    {
+        name: "geometry_gl",
+        project: "Pathfinder 3",
+        projectUrl: "https://github.com/servo/pathfinder",
+        license: "MIT",
+        licenseUrl: "https://github.com/servo/pathfinder/blob/master/LICENSE-MIT",
+        source:
+            "https://github.com/servo/pathfinder/blob/master/gl/src/lib.rs",
+        language: "Rust",
+    },
+];
