eae6497c9c481408caffccd2c8f1c10bd5a0a1b7
Carlos Villar <carlosbetera3dx@gmail.com>
2022-03-25 08:24:30 +0100

    Added more files from FastAPI to challenges

--- challenges/fastapi.js
+++ challenges/fastapi.js
@@ -1,15 +1,79 @@
 module.exports = [
     {
-        name: "param_function.py",
+        name: "applications.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/applications.py",
+    },
+    {
+        name: "background.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/background.py",
+    },
+    {
+        name: "concurrency.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/concurrency.py",
+    },
+    {
+        name: "datastructures.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/datastructures.py",
+    },
+    {
+        name: "encoders.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/encoders.py",
+    },
+    {
+        name: "exception_handlers.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/exception_handlers.py",
+    },
+    {
+        name: "exceptions.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/exceptions.py",
+    },
+    {
+        name: "logger.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/logger.py",
+    },
+    {
+        name: "param_functions.py",
         source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/param_functions.py",
+    },
+    {
+        name: "params.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/params.py",
+    },
+    {
+        name: "requests.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/requests.py",
+    },
+    {
+        name: "responses.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/responses.py",
+    },
+    {
+        name: "routing.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/routing.py",
+    },
+    {
+        name: "staticfiles.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/staticfiles.py",
+    },
+    {
+        name: "templating.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/templating.py",
+    },
+    {
+        name: "testclient.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/testclient.py",
+    },
+    {
+        name: "types.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/types.py",
     },
     {
         name: "utils.py",
         source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/utils.py",
     },
     {
-	name: "routing.py",
-	source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/routing.py",
+        name: "websockets.py",
+        source: "https://raw.githubusercontent.com/tiangolo/fastapi/master/fastapi/websockets.py",
     }
   ].map((file) => ({
     ...file,
