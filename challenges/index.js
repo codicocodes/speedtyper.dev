f85046cac597cd70046d01818c572cb4182488e4
alvand1399 <alvand1399@gmail.com>
2022-04-16 12:40:58 +0430

    Add: Some Vue.js Code Snippets

--- challenges/index.js
+++ challenges/index.js
@@ -1,19 +1,20 @@
-const react = require("./react");
-const agile = require("./agile");
-const jotling = require("./jotling");
-const lodash = require("./lodash");
-const narutoql = require("./narutoql");
-const sfss = require("./sfss");
-const pdfSigner = require("./pdf-signer");
-const theAlgorithms = require("./the-algorithms");
-const unirestJava = require("./unirest-java");
-const mixin = require("./mixin");
-const wordokuCsp = require("./wordoku_csp.js");
-const configr = require("./configr");
-const libdeflate = require("./libdeflate");
-const pathfinder = require("./pathfinder");
-const fastapi = require("./fastapi");
-const pytest = require("./pytest");
+const react = require('./react');
+const agile = require('./agile');
+const jotling = require('./jotling');
+const lodash = require('./lodash');
+const narutoql = require('./narutoql');
+const sfss = require('./sfss');
+const pdfSigner = require('./pdf-signer');
+const theAlgorithms = require('./the-algorithms');
+const unirestJava = require('./unirest-java');
+const mixin = require('./mixin');
+const wordokuCsp = require('./wordoku_csp.js');
+const configr = require('./configr');
+const libdeflate = require('./libdeflate');
+const pathfinder = require('./pathfinder');
+const fastapi = require('./fastapi');
+const pytest = require('./pytest');
+const vue = require('./vue');
 
 module.exports = [
   ...lodash,
@@ -32,4 +33,5 @@ module.exports = [
   ...pathfinder,
   ...fastapi,
   ...pytest,
+  ...vue,
 ];
---
+++ challenges/vue.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    name: 'scripts_filter-e2e',
+    project: 'vuejs/core',
+    projectUrl: 'https://github.com/vuejs/core',
+    license: 'MIT',
+    licenseUrl: 'https://github.com/vuejs/core/blob/main/LICENSE',
+    source:
+      'https://raw.githubusercontent.com/vuejs/core/main/scripts/filter-e2e.js',
+    language: 'JavaScript',
+  },
+  {
+    name: 'scripts_filter-unit',
+    project: 'vuejs/core',
+    projectUrl: 'https://github.com/vuejs/core',
+    license: 'MIT',
+    licenseUrl: 'https://github.com/vuejs/core/blob/main/LICENSE',
+    source:
+      'https://raw.githubusercontent.com/vuejs/core/main/scripts/filter-unit.js',
+    language: 'JavaScript',
+  },
+  {
+    name: 'scripts_preinstall',
+    project: 'vuejs/core',
+    projectUrl: 'https://github.com/vuejs/core',
+    license: 'MIT',
+    licenseUrl: 'https://github.com/vuejs/core/blob/main/LICENSE',
+    source:
+      'https://raw.githubusercontent.com/vuejs/core/main/scripts/preinstall.js',
+    language: 'JavaScript',
+  },
+];
