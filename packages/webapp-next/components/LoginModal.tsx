abd44cea61d3229b0684ddfaf4bcde816dd66502
alcherith <alcherith@gmail.com>
2022-09-08 00:35:53 +0300

    Close the login modal when user clicks outside the modal

--- packages/webapp-next/components/LoginModal.tsx
+++ packages/webapp-next/components/LoginModal.tsx
@@ -22,8 +22,12 @@ const LoginModal = () => {
 
       {showModal ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            onClick={() => setShowModal(false)}
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl"
+              onClick={(e) => e.stopPropagation()}
+            >
               <div className="border-0 lg:rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 <div className="flex justify-center items-center p-5 rounded-t">
                   <button
