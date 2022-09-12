653df9073ee27d03f8a92437051094442ac21e23
codico <codicocodes@gmail.com>
2022-09-13 01:16:13 +0200

    Refactor GithubHandler

--- packages/backend/src/api/auth/githubHandler.ts
+++ packages/backend/src/api/auth/githubHandler.ts
@@ -2,14 +2,16 @@ import { Request, Response } from "express";
 
 const { GITHUB_CLIENT_ID, SERVER_URI } = process.env;
 
-export default async (req: Request, res: Response) => {
-  const currentUrl = req.query.currentUrl?.toString() || "";
+const GITHUB_REDIRECT_URI = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=read:user&redirect_uri=${SERVER_URI}/auth/github/callback`;
 
+export default async (req: Request, res: Response) => {
+  saveLoginReferrer(req);
+  res.redirect(GITHUB_REDIRECT_URI);
+};
+
+function saveLoginReferrer(req: Request) {
+  const currentUrl = req.query.currentUrl?.toString();
   if (req.session) {
     req.session.loginReferrer = currentUrl;
   }
-
-  const githubUri = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=read:user&redirect_uri=${SERVER_URI}/auth/github/callback`;
-
-  res.redirect(githubUri);
-};
+}
