903e11f67ecc67f4c8c0bcad732873b4260f7c54
codico <codicocodes@gmail.com>
2022-09-22 20:59:40 +0200

    gh-31 Fix eslint errors in pingDiscord

--- packages/backend/src/api/pingDiscord.ts
+++ packages/backend/src/api/pingDiscord.ts
@@ -1,28 +1,26 @@
 import { Request, Response } from "express";
 import RaceManager from "RaceManager";
 
-const pingDiscord = (raceManager: RaceManager) => (
-  req: Request,
-  res: Response
-) => {
-  const user = req.session.user;
-  const { raceId } = req.params;
+const pingDiscord =
+  (raceManager: RaceManager) => (req: Request, res: Response) => {
+    const user = req.session.user;
+    const { raceId } = req.params;
 
-  if (!raceId || !user) {
-    return res.status(400).send({ message: "So bad" });
-  }
+    if (!raceId || !user) {
+      return res.status(400).send({ message: "So bad" });
+    }
 
-  const race = raceManager.getRaceById(raceId);
+    const race = raceManager.getRaceById(raceId);
 
-  if (!race) {
-    return res
-      .status(404)
-      .send({ message: "What are you doing giving a race id with no race" });
-  }
+    if (!race) {
+      return res
+        .status(404)
+        .send({ message: "What are you doing giving a race id with no race" });
+    }
 
-  race.pingDiscord(user);
+    race.pingDiscord(user);
 
-  return res.send({ message: "So successful probably" });
-};
+    return res.send({ message: "So successful probably" });
+  };
 
 export default pingDiscord;
