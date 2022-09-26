4df0d6180a8f4af79558dd38d5d8bd20bd2a9d7d
alcherith <86848308+Alcherith@users.noreply.github.com>
2022-09-26 18:32:34 +0300

    improvement on button component

--- packages/webapp-next/components/Button.tsx
+++ packages/webapp-next/components/Button.tsx
@@ -1,48 +1,52 @@
 import React, { FC } from "react";
-// TODO: types
+
 type props = {
     color: "primary" | "secondary",
     disabled?: boolean,
-    onClick?: any,
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void,
     leftIcon?: React.ReactElement,
     rightIcon?: React.ReactElement,
     text: string,
     title: string,
-    size?: "lg",
+    size?: "md" | "lg",
 }
 
-const Button:FC<props> = ({color, disabled, onClick, leftIcon, rightIcon, text, title, size}) => {
-    if (color === "primary") {
-        return (
-            <button
+const Button: FC<props> = ({ color, disabled, onClick, leftIcon, rightIcon, text, title, size = "md" }) => {
+    const sharedStyle = "flex items-center text-gray-900 border-gray-200 border rounded"
+
+    const style = color === "primary"
+        ?
+        `bg-off-white`
+        :
+        `bg-purple-400`
+
+    const disabledStyle = disabled
+        ?
+        "cursor-not-allowed opacity-80"
+        :
+        "cursor-pointer"
+
+    const buttonSize = size == "lg" ?
+        "text-xl px-12 py-2"
+        :
+        "text-base py-1.5 px-2"
+
+    return (
+        <button
             type="button"
             title={title}
             style={{ transition: "all .15s ease" }}
             onClick={onClick}
-            className={`flex hover:bg-gray-100 bg-off-white shadow rounded items-center ${disabled ? "cursor-not-allowed" : "cursor-pointer"} ${ size == "lg" ? "text-xl px-12 py-2 mt-4 mx-12 mb-6" : "text-base px-2 lg:px-4 py-1 mx-2 lg:mx-2"} border-gray-200 border text-gray-900`}
-            >
+            className={`${sharedStyle} ${style} ${disabledStyle} ${buttonSize}`}
+            disabled={disabled}
+            aria-expanded="true"
+            aria-haspopup="true"
+        >
             <>
                 {leftIcon && leftIcon}
                 <p className="pl-1">{text}</p>
                 {rightIcon && rightIcon}
             </>
-            </button>
-        )
-    }
-
-    return (
-        <button
-        type="button"
-        className={`flex my-4 items-center ${disabled ? "cursor-not-allowed" : "hover:bg-purple-300 cursor-pointer"} bg-purple-400 py-2 px-4 ml-4 rounded shadow-2xl border-gray-200 border`}
-        onClick={onClick}
-        aria-expanded="true"
-        aria-haspopup="true"
-        >
-        <>
-            {leftIcon && leftIcon}
-            <p className="pl-1">{text}</p>
-            {rightIcon && rightIcon}
-        </>
         </button>
     )
 }
--- packages/webapp-next/components/LanguageDropdown.tsx
+++ packages/webapp-next/components/LanguageDropdown.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import useLanguages from "../hooks/useLanguages";
 import Button from "./Button";
+import { DownArrowIcon } from "../assets/icons";
 
 const LanguageDropdown = ({
   selectedLanguage,
@@ -12,24 +13,6 @@ const LanguageDropdown = ({
   const [showDropdown, setShowDropdown] = useState(false);
 
   const languages = useLanguages();
-
-  const DownArrowIcon = () => {
-    return (
-      <svg
-        className="-mr-1 ml-2 h-5 w-5"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        aria-hidden="true"
-      >
-        <path
-          fillRule="evenodd"
-          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-          clipRule="evenodd"
-        />
-      </svg>
-    )
-  }
 
   return (
     <div className="relative inline-block text-left">
