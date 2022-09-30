ac9551139bf43f7c81bc048360eebc432238850a
codico <codicocodes@gmail.com>
2022-10-01 01:59:40 +0200

    Only show incorrect bg-red on non whitespace

--- packages/webapp-next/modules/play2/components/IncorrectChars.tsx
+++ packages/webapp-next/modules/play2/components/IncorrectChars.tsx
@@ -1,6 +1,32 @@
 import { useCodeStore } from "../state/code-store";
 
+function isOnlySpace(str: string) {
+  return str.trim().length === 0;
+}
+
 export function IncorrectChars() {
   const incorrectChars = useCodeStore((state) => state.incorrectChars);
-  return <span className="bg-red-500">{incorrectChars()}</span>;
+  const incorrectLines = incorrectChars().split("\n");
+  return (
+    <>
+      {incorrectLines.map((line, index) => {
+        const prefixLineBreak = index !== 0;
+        const lineWithPrefix = prefixLineBreak ? "\n".concat(line) : line;
+        const subline = lineWithPrefix.replace(/\n/g, "↵\n").split(/(\s+)/);
+        return (
+          <>
+            {subline.map((chars) => {
+              const bgColor =
+                isOnlySpace(chars) && chars.length > 1 ? "" : "bg-red-500";
+              return (
+                <span key={index} className={`${bgColor}`}>
+                  {chars}
+                </span>
+              );
+            })}
+          </>
+        );
+      })}
+    </>
+  );
 }
