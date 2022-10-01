abada4e10582ff6437e6b6ae2ceea5858e191283
codico <codicocodes@gmail.com>
2022-10-01 16:18:20 +0200

    Fix repeating key in incorrect chars

--- packages/webapp-next/modules/play2/components/IncorrectChars.tsx
+++ packages/webapp-next/modules/play2/components/IncorrectChars.tsx
@@ -6,27 +6,34 @@ function isOnlySpace(str: string) {
 
 export function IncorrectChars() {
   const incorrectChars = useCodeStore((state) => state.incorrectChars);
-  const incorrectLines = incorrectChars().split("\n");
+  const charGroups = parseIncorrectCharGroups(incorrectChars());
   return (
     <>
-      {incorrectLines.map((line, index) => {
-        const prefixLineBreak = index !== 0;
-        const lineWithPrefix = prefixLineBreak ? "\n".concat(line) : line;
-        const subline = lineWithPrefix.replace(/\n/g, "↵\n").split(/(\s+)/);
+      {charGroups.map((chars, index) => {
+        const bgColor =
+          isOnlySpace(chars) && chars.length > 1 ? "" : "bg-red-500";
         return (
-          <>
-            {subline.map((chars) => {
-              const bgColor =
-                isOnlySpace(chars) && chars.length > 1 ? "" : "bg-red-500";
-              return (
-                <span key={index} className={`${bgColor}`}>
-                  {chars}
-                </span>
-              );
-            })}
-          </>
+          <span key={index} className={`${bgColor}`}>
+            {chars}
+          </span>
         );
       })}
     </>
   );
+}
+
+function parseIncorrectCharGroups(incorrectChars: string) {
+  const incorrectLines = incorrectChars.split("\n").filter(Boolean);
+  const charGroups = incorrectLines
+    .map((line, lineIndex) => {
+      const prefixLineBreak = lineIndex !== 0;
+      const lineWithPrefix = prefixLineBreak ? "\n".concat(line) : line;
+      // TODO: make ↵\n a constant somewhere which can be reused
+      const subline = lineWithPrefix.replace(/\n/g, "↵\n").split(/(\s+)/);
+      return subline.map((chars) => {
+        return chars;
+      });
+    })
+    .flat();
+  return charGroups;
 }
