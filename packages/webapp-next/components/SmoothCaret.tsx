e324aff3d5e7cdb4dc6ed9755e6b704b9b22858d
codico <codicocodes@gmail.com>
2022-10-02 12:05:22 +0200

    Reset original smooth caret

--- packages/webapp-next/components/SmoothCaret.tsx
+++ packages/webapp-next/components/SmoothCaret.tsx
@@ -8,7 +8,7 @@ const SmoothCaret = ({
   inputIsSelected: boolean;
 }) => {
   const left = (nextCharRef.current?.offsetLeft ?? 0) - 2 + "px";
-  const top = (nextCharRef.current?.offsetTop ?? 0) - 2 + "px";
+  const top = (nextCharRef.current?.offsetTop ?? 0) - 5 + "px";
 
   useEffect(() => {
     const element = document.getElementById("caret");
@@ -34,8 +34,8 @@ const SmoothCaret = ({
         inputIsSelected ? "" : "hidden"
       } absolute bg-purple-400 rounded-lg`}
       style={{
-        height: "28px",
-        width: "2px",
+        height: "32px",
+        width: "2.5px",
       }}
     ></div>
   );
