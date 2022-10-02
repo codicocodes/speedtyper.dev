460e3e2f384201e415df4b8d7cbaccedacd6ad02
codico <codicocodes@gmail.com>
2022-10-02 19:29:58 +0200

    Animate Navbar Buttons

--- packages/webapp-next/components/Navbar.tsx
+++ packages/webapp-next/components/Navbar.tsx
@@ -8,7 +8,7 @@ import Button from "../common/components/Button";
 import { TwitchLogo, DiscordLogo } from "../assets/icons";
 import Image from "next/image";
 import { useIsPlaying } from "../common/hooks/useIsPlaying";
-
+import { AnimatePresence, motion } from "framer-motion";
 // TODO: should not be able to be both null and undefined
 // Maybe we should consider putting isGuest on the user object
 // on the server
@@ -28,17 +28,19 @@ const useIsGuest = (user: IUser | null | undefined): boolean => {
 
 const Navbar = () => {
   return (
-    <header>
-      <div className="flex items-center align-center py-2 px-2 sm:px-2 lg:px-40 xl:px-40 lg:py-4 relative">
-        <Link href="/">
-          <span className="flex items-center cursor-pointer trailing-widest leading-normal text-xl  pl-2 text-off-white hover:text-white mr-2 lg:mr-6">
-            <div className="flex items-center mr-4 mb-1">
-              <Logo />
-            </div>
-            <WebsiteName />
-          </span>
-        </Link>
-        <NavBarButtons />
+    <header className="relative">
+      <div className="absolute w-full">
+        <div className="flex items-start py-2 px-2 sm:px-2 lg:px-40 xl:px-40 lg:py-4">
+          <Link href="/">
+            <span className="flex items-center cursor-pointer trailing-widest leading-normal text-xl  pl-2 text-off-white hover:text-white mr-2 lg:mr-6">
+              <div className="flex items-center mr-4 mb-1">
+                <Logo />
+              </div>
+              <WebsiteName />
+            </span>
+          </Link>
+          <NavBarButtons />
+        </div>
       </div>
     </header>
   );
@@ -83,30 +85,42 @@ const NavBarButtons = () => {
   const userIsGuest = useIsGuest(user);
   const isUserLoggedIn = !userIsGuest && user && logout;
   const isPlaying = useIsPlaying();
-  return isPlaying ? null : (
-    <div className="flex-grow flex items-center gap-4">
-      <div className="text-sm flex-grow"></div>
-      {userIsGuest && <LoginModal />}
-      {isUserLoggedIn && <AvatarModal user={user} logout={logout} />}
-      <a href="https://discord.gg/AMbnnN5eep" target="blank">
-        <Button
-          color="primary"
-          leftIcon={<DiscordLogo />}
-          size="sm"
-          title="Join the discord"
-          text="Join"
-        />
-      </a>
-      <a href="https://twitch.tv/codico" target="blank">
-        <Button
-          color="primary"
-          size="sm"
-          leftIcon={<TwitchLogo />}
-          title="Follow codico on twitch"
-          text="Follow"
-        />
-      </a>
-    </div>
+  return (
+    <>
+      {!isPlaying && (
+        <AnimatePresence>
+          <motion.div
+            className="flex-grow flex items-center gap-4"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="text-sm flex-grow"></div>
+            {userIsGuest && <LoginModal />}
+            {isUserLoggedIn && <AvatarModal user={user} logout={logout} />}
+            <a href="https://discord.gg/AMbnnN5eep" target="blank">
+              <Button
+                color="primary"
+                leftIcon={<DiscordLogo />}
+                size="sm"
+                title="Join the discord"
+                text="Join"
+              />
+            </a>
+            <a href="https://twitch.tv/codico" target="blank">
+              <Button
+                color="primary"
+                size="sm"
+                leftIcon={<TwitchLogo />}
+                title="Follow codico on twitch"
+                text="Follow"
+              />
+            </a>
+          </motion.div>
+        </AnimatePresence>
+      )}
+    </>
   );
 };
 
