e75efdc62cc40c63d8202cc10296261aee21d022
codico <codicocodes@gmail.com>
2022-10-02 19:18:01 +0200

    Fade navbar when typing

--- packages/webapp-next/components/Navbar.tsx
+++ packages/webapp-next/components/Navbar.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import Button from "../common/components/Button";
 import { TwitchLogo, DiscordLogo } from "../assets/icons";
 import Image from "next/image";
+import { useIsPlaying } from "../common/hooks/useIsPlaying";
 
 // TODO: should not be able to be both null and undefined
 // Maybe we should consider putting isGuest on the user object
@@ -26,49 +27,86 @@ const useIsGuest = (user: IUser | null | undefined): boolean => {
 };
 
 const Navbar = () => {
-  const { user, logout } = useAppContext();
-  const websiteName = "speedtyper";
-  const userIsGuest = useIsGuest(user);
-  const isUserLoggedIn = !userIsGuest && user && logout;
-
   return (
     <header>
       <div className="flex items-center align-center py-2 px-2 sm:px-2 lg:px-40 xl:px-40 lg:py-4 relative">
         <Link href="/">
           <span className="flex items-center cursor-pointer trailing-widest leading-normal text-xl  pl-2 text-off-white hover:text-white mr-2 lg:mr-6">
             <div className="flex items-center mr-4 mb-1">
-              <Image width="45px" height="25px" src="/logo.png" quality={100} />
+              <Logo />
             </div>
-            <h2 className="hidden sm:block whitespace-no-wrap font-bold">
-              {websiteName}
-            </h2>
+            <WebsiteName />
           </span>
         </Link>
-        <div className="flex-grow flex items-center gap-4">
-          <div className="text-sm flex-grow"></div>
-          {userIsGuest && <LoginModal />}
-          {isUserLoggedIn && <AvatarModal user={user} logout={logout} />}
-          <a href="https://discord.gg/AMbnnN5eep" target="blank">
-            <Button
-              color="primary"
-              leftIcon={<DiscordLogo />}
-              size="sm"
-              title="Join the discord"
-              text="Join"
-            />
-          </a>
-          <a href="https://twitch.tv/codico" target="blank">
-            <Button
-              color="primary"
-              size="sm"
-              leftIcon={<TwitchLogo />}
-              title="Follow codico on twitch"
-              text="Follow"
-            />
-          </a>
-        </div>
+        <NavBarButtons />
       </div>
     </header>
+  );
+};
+
+const WebsiteName = () => {
+  const isPlaying = useIsPlaying();
+  const websiteName = "speedtyper";
+  const colorClass = isPlaying ? "text-faded-gray" : "text-inherit";
+  return (
+    <h2
+      className={`hidden sm:block whitespace-no-wrap font-bold ${colorClass}`}
+    >
+      {websiteName}
+    </h2>
+  );
+};
+
+const Logo = () => {
+  const isPlaying = useIsPlaying();
+  return isPlaying ? (
+    <Image
+      width="45px"
+      height="25px"
+      src="/faded-logo.png"
+      quality={100}
+      alt="speedtyper logo"
+    />
+  ) : (
+    <Image
+      width="45px"
+      height="25px"
+      src="/logo.png"
+      quality={100}
+      alt="speedtyper logo"
+    />
+  );
+};
+
+const NavBarButtons = () => {
+  const { user, logout } = useAppContext();
+  const userIsGuest = useIsGuest(user);
+  const isUserLoggedIn = !userIsGuest && user && logout;
+  const isPlaying = useIsPlaying();
+  return isPlaying ? null : (
+    <div className="flex-grow flex items-center gap-4">
+      <div className="text-sm flex-grow"></div>
+      {userIsGuest && <LoginModal />}
+      {isUserLoggedIn && <AvatarModal user={user} logout={logout} />}
+      <a href="https://discord.gg/AMbnnN5eep" target="blank">
+        <Button
+          color="primary"
+          leftIcon={<DiscordLogo />}
+          size="sm"
+          title="Join the discord"
+          text="Join"
+        />
+      </a>
+      <a href="https://twitch.tv/codico" target="blank">
+        <Button
+          color="primary"
+          size="sm"
+          leftIcon={<TwitchLogo />}
+          title="Follow codico on twitch"
+          text="Follow"
+        />
+      </a>
+    </div>
   );
 };
 
