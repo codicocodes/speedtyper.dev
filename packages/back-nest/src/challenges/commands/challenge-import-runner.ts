39947a71d6765deeb1026b6f093741d1fdcd7cc0
codico <codicocodes@gmail.com>
2022-10-09 11:28:57 +0200

    Refactor ChallengeImportRunner

--- packages/back-nest/src/challenges/commands/challenge-import-runner.ts
+++ packages/back-nest/src/challenges/commands/challenge-import-runner.ts
@@ -1,6 +1,7 @@
 import { Command, CommandRunner } from 'nest-commander';
 import { GithubAPI } from 'src/connectors/github/services/github-api';
 import { Challenge } from '../entities/challenge.entity';
+import { UnsyncedFile } from '../entities/unsynced-file.entity';
 import { ChallengeService } from '../services/challenge.service';
 import { ParserService } from '../services/parser.service';
 import { UnsyncedFileService } from '../services/unsynced-file.service';
@@ -20,24 +21,29 @@ export class ChallengeImportRunner extends CommandRunner {
     super();
   }
   async run(): Promise<void> {
-    const files = await this.unsynced.findAllWithProject();
     let filesSynced = 0;
+    const files = await this.unsynced.findAllWithProject();
     for (const file of files) {
-      const blob = await this.api.fetchBlob(
-        file.project.fullName,
-        file.currentSha,
-      );
-      const nodes = this.parseNodesFromContent(file.path, blob.content);
-      const challenges = nodes.map((node) =>
-        Challenge.fromTSNode(file.project, file, node),
-      );
-      await this.challengeService.upsert(challenges);
-      await this.unsynced.remove([file]);
+      const challenges = await this.syncChallengesFromFile(file);
       filesSynced++;
       console.info(
         `[challenge-import]: ${filesSynced}/${files.length} synced. Challenges added=${challenges.length}`,
       );
     }
+  }
+
+  private async syncChallengesFromFile(file: UnsyncedFile) {
+    const blob = await this.api.fetchBlob(
+      file.project.fullName,
+      file.currentSha,
+    );
+    const nodes = this.parseNodesFromContent(file.path, blob.content);
+    const challenges = nodes.map((node) =>
+      Challenge.fromTSNode(file.project, file, node),
+    );
+    await this.challengeService.upsert(challenges);
+    await this.unsynced.remove([file]);
+    return challenges;
   }
 
   private parseNodesFromContent(path: string, base64Content: string) {
