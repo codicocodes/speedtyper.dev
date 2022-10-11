1a6fb45515c269a64c08947aee3fe892e3cf7cf3
codico <codicocodes@gmail.com>
2022-10-11 21:44:19 +0200

    Support showing days played in toHumanReadable

--- packages/webapp-next/common/utils/toHumanReadableTime.ts
+++ packages/webapp-next/common/utils/toHumanReadableTime.ts
@@ -1,10 +1,28 @@
+const MINUTES_IN_SECONDS = 60;
+
+const HOURS_IN_SECONDS = MINUTES_IN_SECONDS * 60;
+
+const DAYS_IN_SECONDS = HOURS_IN_SECONDS * 24;
+
 export const toHumanReadableTime = (secs: number) => {
-  const hours = Math.floor(secs / 3600);
-  const minutes = Math.floor((secs - hours * 3600) / 60) % 60;
-  const seconds = secs - hours * 3600 - minutes * 60;
+  const days = Math.floor(secs / DAYS_IN_SECONDS);
+  const hours = Math.floor((secs - days * DAYS_IN_SECONDS) / HOURS_IN_SECONDS);
+
+  const minutes =
+    Math.floor(
+      (secs - days * DAYS_IN_SECONDS - hours * HOURS_IN_SECONDS) / 60
+    ) % MINUTES_IN_SECONDS;
+
+  const seconds =
+    secs -
+    days * DAYS_IN_SECONDS -
+    hours * HOURS_IN_SECONDS -
+    minutes * MINUTES_IN_SECONDS;
+
   let result = "";
+  result = days ? result.concat(`${days} days `) : result;
   result = hours ? result.concat(`${hours}h `) : result;
-  result = minutes ? result.concat(`${minutes}m `) : result;
-  result = result.concat(`${seconds}s`);
-  return result;
+  if (!days) result = minutes ? result.concat(`${minutes}m `) : result;
+  if (!hours) result = result.concat(`${seconds}s`);
+  return result.trim();
 };
