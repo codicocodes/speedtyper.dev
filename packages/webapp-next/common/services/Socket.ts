31404d7b4de5bc16ea499018e6cce142d5ea3221
codico <codicocodes@gmail.com>
2022-10-16 03:00:16 +0200

    Update socket latest to use methods

--- packages/webapp-next/common/services/Socket.ts
+++ packages/webapp-next/common/services/Socket.ts
@@ -1,25 +1,28 @@
-import { io, Socket as SocketIOSocket } from "socketio-latest";
+import { connect, Socket as SocketIOSocket } from "socketio-latest";
 
 export default class SocketLatest {
   socket: SocketIOSocket;
 
   constructor(serverUrl: string) {
-    this.socket = io(serverUrl, { withCredentials: true });
+    this.socket = connect(serverUrl, {
+      withCredentials: true,
+      reconnection: true,
+    });
   }
 
-  disconnect = () => {
+  disconnect() {
     this.socket.emit("disconnect");
     if (this.socket) this.socket.disconnect();
-  };
+  }
 
-  subscribe = (event: string, cb: (error: string | null, msg: any) => void) => {
+  subscribe(event: string, cb: (error: string | null, msg: any) => void) {
     if (!this.socket) return true;
     this.socket.on(event, (msg: any) => {
       return cb(null, msg);
     });
-  };
+  }
 
-  emit = (event: string, data?: any) => {
+  emit(event: string, data?: any) {
     if (this.socket) this.socket.emit(event, data);
-  };
+  }
 }
