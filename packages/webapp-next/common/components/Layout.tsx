f62b7a777fcaf7f1e6f3940d2ed1fd55359bafe8
codico <codicocodes@gmail.com>
2022-11-13 14:02:08 +0100

    Extract container component

--- packages/webapp-next/common/components/Layout.tsx
+++ packages/webapp-next/common/components/Layout.tsx
@@ -4,20 +4,34 @@ interface LayoutProps {
   children: JSX.Element;
 }
 
+interface ContainerProps {
+  children: JSX.Element;
+  centered: boolean;
+}
+
+export function Container({ children, centered }: ContainerProps) {
+  return (
+    <div
+      className={`flex justify-center h-full w-full 
+     ${centered ? "items-center justify-content" : ""} `}
+    >
+      <div
+        className="flex flex-col max-w-5xl w-full"
+        style={{
+          width: "920px",
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
+
 export function Layout({ children }: LayoutProps) {
   return (
     <>
       <Navbar />
-      <div className="flex items-center justify-center h-full w-full">
-        <div
-          className="flex flex-col max-w-5xl items-center justify-center w-full"
-          style={{
-            width: "920px",
-          }}
-        >
-          {children}
-        </div>
-      </div>
+      <Container centered={true}>{children}</Container>
     </>
   );
 }
