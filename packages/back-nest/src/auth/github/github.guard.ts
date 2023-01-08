7dc72c304e30ad76bfbcf3cf198565044f8980bb
codico <codicocodes@gmail.com>
2023-01-08 20:37:10 +0100

    Update auth module

--- packages/back-nest/src/auth/github/github.guard.ts
+++ packages/back-nest/src/auth/github/github.guard.ts
@@ -1,20 +1,25 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard, IAuthModuleOptions } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Injectable()
 export class GithubOauthGuard extends AuthGuard('github') {
   getAuthenticateOptions(context: ExecutionContext): IAuthModuleOptions {
-    const request = context.switchToHttp().getRequest();
-    const { next } = request.query;
+    const request = context.switchToHttp().getRequest<Request>();
+    return {
+      state: this.getState(request),
+      session: true,
+    };
+  }
+
+  getState(request: Request) {
+    const { next } = request.query as Record<string, string>;
     const queryParams = next
       ? {
           next,
         }
       : {};
     const state = new URLSearchParams(queryParams).toString();
-    return {
-      state,
-      session: true,
-    };
+    return state;
   }
 }
