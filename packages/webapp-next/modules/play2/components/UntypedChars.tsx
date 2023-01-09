7591ff6d8581d13c18451010e193802280c9095c
codico <codicocodes@gmail.com>
2023-01-09 03:07:33 +0100

    Add split char

--- packages/webapp-next/modules/play2/components/UntypedChars.tsx
+++ packages/webapp-next/modules/play2/components/UntypedChars.tsx
@@ -1,6 +1,16 @@
 import { useCodeStore } from "../state/code-store";
-
+const splitChar = "‎";
 export function UntypedChars() {
   const untypedChars = useCodeStore((state) => state.untypedChars);
-  return <span>{untypedChars()}</span>;
+  return (
+    <>
+      {untypedChars()
+        .split("")
+        .join(splitChar)
+        .split("")
+        .map((c, i) => (
+          <span key={i}>{c}</span>
+        ))}
+    </>
+  );
 }
