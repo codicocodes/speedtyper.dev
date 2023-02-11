deb95ef0a1e1afcc42938c118936e5facd72772e
codico <codicocodes@gmail.com>
2023-02-11 18:54:38 +0100

    Update DB env variables

--- packages/back-nest/src/config/postgres.ts
+++ packages/back-nest/src/config/postgres.ts
@@ -2,9 +2,9 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: __dirname + '/../../.env' });
 
 export const pgOptions = {
-  host: process.env.PG_HOST,
-  port: parseInt(process.env.PG_PORT, 10),
-  username: process.env.PG_USERNAME,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
+  host: process.env.PGHOST,
+  port: parseInt(process.env.PGPORT, 10),
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
 };
