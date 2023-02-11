fb37da08212d073bf48bd048ef38cd08b0ba64e5
codico <codicocodes@gmail.com>
2023-02-11 22:14:07 +0100

    Add type to socketCors

--- packages/back-nest/src/config/cors.ts
+++ packages/back-nest/src/config/cors.ts
@@ -1,10 +1,12 @@
+import { GatewayMetadata } from '@nestjs/websockets';
+
 export const getAllowedOrigins = () => {
   return process.env.NODE_ENV === 'production'
     ? ['https://speedtyper.dev', 'https://www.speedtyper.dev']
     : ['http://localhost:3001'];
 };
 
-export const socketCors = {
+export const socketCors: GatewayMetadata = {
   cors: {
     origin: getAllowedOrigins(),
     methods: ['GET', 'POST'],
