1a6069916045ea68cfcf602c1b4b942d52ef974d
codico <codicocodes@gmail.com>
2023-02-11 14:26:12 +0100

    Cleanup github strategy

--- packages/back-nest/src/auth/github/github.strategy.ts
+++ packages/back-nest/src/auth/github/github.strategy.ts
@@ -3,14 +3,10 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Profile, Strategy } from 'passport-github';
 import { UserService } from 'src/users/services/user.service';
-import { User } from 'src/users/entities/user.entity';
 import { UpsertGithubUserDTO } from 'src/users/entities/upsertGithubUserDTO';
 
 @Injectable()
 export class GithubStrategy extends PassportStrategy(Strategy, 'github') {
-  // TODO: complete this class
-  // 1. fix constructor
-  // 2. fix validator
   constructor(cfg: ConfigService, private userService: UserService) {
     super({
       clientID: cfg.get<string>('GITHUB_CLIENT_ID'),
