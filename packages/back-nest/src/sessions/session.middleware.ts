dbdef915080521d55f3438af85cf3ea8649b8abf
codico <codicocodes@gmail.com>
2023-02-12 23:16:15 +0100

    Enable sameSite lax for github sso

--- packages/back-nest/src/sessions/session.middleware.ts
+++ packages/back-nest/src/sessions/session.middleware.ts
@@ -19,7 +19,7 @@ export const getSessionMiddleware = () => {
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      sameSite: 'strict',
+      sameSite: 'lax',
       secure: process.env.NODE_ENV === 'production',
       maxAge: ONE_DAY,
       ...(process.env.NODE_ENV === 'production'
