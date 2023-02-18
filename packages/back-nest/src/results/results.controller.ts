6deab25fdbaca67b78102acba80022cde17ae666
codico <codicocodes@gmail.com>
2023-02-18 12:07:19 +0100

    Cache leaderboard promises

--- packages/back-nest/src/results/results.controller.ts
+++ packages/back-nest/src/results/results.controller.ts
@@ -4,9 +4,19 @@ import { ResultService } from './services/results.service';
 
 @Controller('results')
 export class ResultsController {
+  leaderboardP?: Promise<LeaderBoardResult[]>;
   constructor(private resultsService: ResultService) {}
   @Get('leaderboard')
-  getLeaderboard(): Promise<LeaderBoardResult[]> {
-    return this.resultsService.getLeaderboard();
+  async getLeaderboard(): Promise<LeaderBoardResult[]> {
+    if (this.leaderboardP) {
+      // there is an ongoing promise
+      return this.leaderboardP;
+    }
+    // cache the leaderboard promise so we only hit the DB once per concurrent request
+    this.leaderboardP = this.resultsService.getLeaderboard();
+    return this.leaderboardP.finally(() => {
+      // reset the promise so new clients don't get a stale leaderboard
+      this.leaderboardP = undefined;
+    });
   }
 }
