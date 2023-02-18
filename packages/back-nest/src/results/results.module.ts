c72b0e8a14c051892914ba1292188b9710c053a0
codico <codicocodes@gmail.com>
2023-02-18 01:21:33 +0100

    Add results controller to module

--- packages/back-nest/src/results/results.module.ts
+++ packages/back-nest/src/results/results.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Result } from './entities/result.entity';
+import { ResultsController } from './results.controller';
 import { ResultCalculationService } from './services/result-calculation.service';
 import { ResultFactoryService } from './services/result-factory.service';
 import { ResultService } from './services/results.service';
@@ -8,6 +9,7 @@ import { ResultService } from './services/results.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Result])],
   providers: [ResultService, ResultFactoryService, ResultCalculationService],
+  controllers: [ResultsController],
   exports: [ResultService, ResultFactoryService],
 })
 export class ResultsModule {}
