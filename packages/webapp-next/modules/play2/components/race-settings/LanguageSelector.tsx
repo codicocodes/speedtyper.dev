4963364352b0235e0116e8c32cec778b70d61375
codico <codicocodes@gmail.com>
2023-02-19 16:42:49 +0100

    Add dropdown icon

--- packages/webapp-next/modules/play2/components/race-settings/LanguageSelector.tsx
+++ packages/webapp-next/modules/play2/components/race-settings/LanguageSelector.tsx
@@ -28,13 +28,26 @@ export function LanguageSelector() {
       </h2>
       <Listbox value={selectedLanguage} onChange={selectProgrammingLanguage}>
         <div className="flex items-center">
-          <Listbox.Button className="bg-gray-200 p-1 w-full rounded">
+          <Listbox.Button className="flex items-center justify-between px-2 bg-gray-200 p-1 w-full rounded">
             {selectedLanguage || "nothing selected"}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+              className="h-5 w-5 text-gray-400"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
           </Listbox.Button>
           {selectedLanguage && (
             <button
               onClick={() => selectProgrammingLanguage(null)}
-              className="flex items-center p-1 h-full  bg-gray-200 ml-2 rounded"
+              className="flex items-center p-1 h-full  bg-gray-200 mx-2 rounded"
             >
               <div className="w-2 text-red-500 fill-current mx-1">
                 <CrossIcon />
