8725640819cf5da7ab23289fe6ea28464a8ac0e8
codico <codicocodes@gmail.com>
2023-02-21 00:26:34 +0100

    Make language required

--- packages/back-nest/src/challenges/entities/challenge.entity.ts
+++ packages/back-nest/src/challenges/entities/challenge.entity.ts
@@ -20,7 +20,7 @@ export class Challenge {
   sha: string;
   @Column({ select: false })
   treeSha: string;
-  @Column({ nullable: true })
+  @Column()
   language: string;
   @Column()
   path: string;
