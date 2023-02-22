4e4cce0d2508e9c88b8ae9b401f1a6be9c8fc1f4
codico <codicocodes@gmail.com>
2023-02-22 19:46:53 +0100

    Remove internal api from legacy backend

--- packages/backend/src/api/internal/internalApi.ts
+++
@@ -1,7 +0,0 @@
-import { internalApiFactory } from "./utils/internalApiFactory";
-import User from "../../models/user";
-import ChallengeResult from "../../models/challengeResult";
-
-export const streamAllUsers = internalApiFactory(User);
-
-export const streamAllResults = internalApiFactory(ChallengeResult);
