ec319e208bbed7bdb149b383b87f7bd6787e79f4
codico <codicocodes@gmail.com>
2023-02-22 21:21:29 +0100

    Add script to export data to new server

---
+++ packages/backend/src/internal-migration/results.ts
@@ -0,0 +1,44 @@
+import challengeResult, { ChallengeResultDoc } from "../models/challengeResult";
+import fetch from "node-fetch";
+import mongoose from "mongoose";
+
+export async function* getResultBatches() {
+  const batchSize = 500;
+  const cursor = challengeResult.find({}).cursor({ batchSize });
+  let resultsBatch = [];
+  for await (const result of cursor) {
+    const r: any = result.toJSON();
+    r.__createdAt = mongoose.Types.ObjectId(result._id).getTimestamp();
+    resultsBatch.push(r);
+    if (resultsBatch.length === batchSize) {
+      yield resultsBatch;
+      resultsBatch = [];
+    }
+  }
+  if (resultsBatch.length > 0) {
+    yield resultsBatch;
+  }
+}
+
+export async function exportResultBatches() {
+  for await (const resultBatch of getResultBatches()) {
+    await sendResults(resultBatch);
+  }
+}
+
+export const INTERNAL_BASE_URL =
+  process.env.INTERNAL_API_BASE_URL ?? "http://localhost:1337/api/internal";
+
+export const RESULTS_API = `${INTERNAL_BASE_URL}/results`;
+
+export async function sendResults(results: ChallengeResultDoc[]) {
+  const apiToken = process.env.INTERNAL_API_TOKEN ?? "";
+  await fetch(RESULTS_API, {
+    method: "POST",
+    body: JSON.stringify(results),
+    headers: {
+      "Content-Type": "application/json",
+      "api-token": apiToken,
+    },
+  }).then((res) => res.json());
+}
---
+++ packages/backend/src/tools/internal/exportResultsRunner.ts
@@ -0,0 +1,14 @@
+import path from "path";
+import { exportResultBatches } from "../../internal-migration/results";
+import { connectToDB } from "../../db";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+connectToDB()
+  .then(exportResultBatches)
+  .then(() => process.exit(0))
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  });
