62e3c0ae201023f30e6d4527d5f2cd191b77c241
codico <codicocodes@gmail.com>
2023-02-27 01:03:23 +0100

    Fix joining public races

--- packages/webapp-next/modules/play2/hooks/useChallenge.ts
+++ packages/webapp-next/modules/play2/hooks/useChallenge.ts
@@ -27,17 +27,30 @@ export function useChallenge(): ChallengeInfo {
   const socket = useConnectionStore((s) => s.socket);
 
   useEffect(() => {
-    socket?.subscribe("challenge_selected", (_, data) => {
+    socket?.subscribe("challenge_selected", (_, challenge) => {
       setChallenge({
         loaded: true,
-        projectName: data.project.fullName,
-        url: data.url,
-        code: data.content,
-        language: data.project.language,
-        filePath: data.path,
-        license: data.project.licenseName,
+        projectName: challenge.project.fullName,
+        url: challenge.url,
+        code: challenge.content,
+        language: challenge.project.language,
+        filePath: challenge.path,
+        license: challenge.project.licenseName,
       });
-      initialize(data.content.replaceAll("\t", "  "));
+      initialize(challenge.content.replaceAll("\t", "  "));
+    });
+    socket?.subscribe("race_joined", (_, raceData) => {
+      const { challenge } = raceData;
+      setChallenge({
+        loaded: true,
+        projectName: challenge.project.fullName,
+        url: challenge.url,
+        code: challenge.content,
+        language: challenge.project.language,
+        filePath: challenge.path,
+        license: challenge.project.licenseName,
+      });
+      initialize(challenge.content.replaceAll("\t", "  "));
     });
   }, [socket, initialize]);
 
